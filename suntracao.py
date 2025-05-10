c134daf40268552280217fa8c9d579eebbb85223
nascimento3322 <vn62511@gmail.com>
2025-05-09 23:54:12 -0300

    Subtração da maneira certa

---
+++ suntracao.py
@@ -0,0 +1,4 @@
+h = int(input("Digite um número para que passamos fazer a subtração: "))
+i = int(input("Digite o outro número para completar: "))
+subtracao = h - i 
+print(f"O resultado final dessa conta é: {subtracao})
