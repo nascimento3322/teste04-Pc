35e16c345f02b079046cb2051381923e5cd2df6f
nascimento3322 <vn62511@gmail.com>
2025-05-09 23:04:49 -0300

    Multiplicando

---
+++ multiplicação.py
@@ -0,0 +1,4 @@
+c = int(input("Olá, digite um número para fazermos a multiplicação: "))
+d = int(input("Ainda falta outro número, por favor digite mais um: "))
+multiplicação = c * d 
+print(f"O resultado final desse calculo é: {multiplicação}")
